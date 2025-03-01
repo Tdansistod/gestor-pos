bec01170788f9adebcf17e4ab182882f7c85a650
lautaro <lautaronuriel@gmail.com>
2025-03-01 15:28:55 -0300

    css routes test

--- swup-config.js
+++ swup-config.js
@@ -1,37 +1,33 @@
 const swup = new Swup();
 
 const setStyle = (url = window.location.pathname) => {
-  // Asegurarse de que la ruta no tenga el slash al final
-  const path = url.endsWith("/") ? url.slice(0, -1) : url;
+  const path = url.split("/").pop().replace(".html", "");
 
-  console.log(path);
-
-  // Esto asegura que si estás en GitHub Pages, las rutas se interpreten correctamente.
-  if (path === "/" || path === "/index.html") {
+  if (path === "" || path === "index") {
     document.getElementById("/")?.classList.add("active");
   } else {
     document.getElementById("/")?.classList.remove("active");
   }
 
-  if (path === "/inventory") {
+  if (path === "inventory") {
     document.getElementById("inventory")?.classList.add("active");
   } else {
     document.getElementById("inventory")?.classList.remove("active");
   }
 
-  if (path === "/supplier") {
+  if (path === "supplier") {
     document.getElementById("supplier")?.classList.add("active");
   } else {
     document.getElementById("supplier")?.classList.remove("active");
   }
 
-  if (path === "/clients") {
+  if (path === "clients") {
     document.getElementById("clients")?.classList.add("active");
   } else {
     document.getElementById("clients")?.classList.remove("active");
   }
 
-  if (path === "/history") {
+  if (path === "history") {
     document.getElementById("history")?.classList.add("active");
   } else {
     document.getElementById("history")?.classList.remove("active");
