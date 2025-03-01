216c7297af5e75c675ada0f2950549ae64056d4d
lautaro <lautaronuriel@gmail.com>
2025-03-01 15:17:55 -0300

    css routes adjust

--- swup-config.js
+++ swup-config.js
@@ -1,22 +1,41 @@
 const swup = new Swup();
 
 const setStyle = (url = window.location.pathname) => {
-  url === "/" || url === "/index.html"
-    ? document.getElementById("/")?.classList.add("active")
-    : document.getElementById("/")?.classList.remove("active");
-  url === "/inventory.html"
-    ? document.getElementById("inventory")?.classList.add("active")
-    : document.getElementById("inventory")?.classList.remove("active");
-  url === "/supplier.html"
-    ? document.getElementById("supplier")?.classList.add("active")
-    : document.getElementById("supplier")?.classList.remove("active");
-  url === "/clients.html"
-    ? document.getElementById("clients")?.classList.add("active")
-    : document.getElementById("clients")?.classList.remove("active");
-  url === "/history.html"
-    ? document.getElementById("history")?.classList.add("active")
-    : document.getElementById("history")?.classList.remove("active");
+  // Asegurarse de que la ruta no tenga el slash al final
+  const path = url.endsWith("/") ? url.slice(0, -1) : url;
+
+  // Esto asegura que si estás en GitHub Pages, las rutas se interpreten correctamente.
+  if (path === "/" || path === "/index.html") {
+    document.getElementById("/")?.classList.add("active");
+  } else {
+    document.getElementById("/")?.classList.remove("active");
+  }
+
+  if (path === "/inventory") {
+    document.getElementById("inventory")?.classList.add("active");
+  } else {
+    document.getElementById("inventory")?.classList.remove("active");
+  }
+
+  if (path === "/supplier") {
+    document.getElementById("supplier")?.classList.add("active");
+  } else {
+    document.getElementById("supplier")?.classList.remove("active");
+  }
+
+  if (path === "/clients") {
+    document.getElementById("clients")?.classList.add("active");
+  } else {
+    document.getElementById("clients")?.classList.remove("active");
+  }
+
+  if (path === "/history") {
+    document.getElementById("history")?.classList.add("active");
+  } else {
+    document.getElementById("history")?.classList.remove("active");
+  }
 };
+
 setStyle();
 
 swup.hooks.on("page:view", (visit) => {
